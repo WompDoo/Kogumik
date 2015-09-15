33fc6e20bdb3524c807d7a500273d208156d6489
Janre <janre.press@khk.ee>
2015-09-15 09:35:32 +0300

    Avaleht [Delivers #102580766]

---
+++ Avaleht Heigole.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link type="text/css" rel="stylesheet" href="Avaleht.css" />
+    <meta charset="UTF-8">
+    <title>Heigo Kure Leht</title>
+</head>
+<!--Avaleht rohelist v2rvi-->
+<body>
+<h1 align="center"><img src="https://s-media-cache-ak0.pinimg.com/736x/e8/ea/0d/e8ea0d273f49a2c99eb88ab66b295fba.jpg"  align="right" width="150px" height="150px"/><u>Heigo Kure Tutvustav Koduleht</u></h1>
+<h3><a href="https://www.facebook.com/heigo.kure?fref=ts" style=color:red>Heigo Kure FB</a></h3>
+<div id="one"><ul>
+    <li>Olen sündinud 24.06.1995</li>
+    <li>Kasvanud Tartus</li>
+    <li>Endine kool Tartu Descartes'i Lütseum</li>
+        <li>Praegune kool Tartu Kutsehariduskeskus</li>
+</ul>
+</div>
+<h4>Hobid, tegevused:</h4>
+<div id="two"><ol>
+    <li><img src="http://www.animatedimages.org/data/media/159/animated-basketball-image-0029.gif" align="right" width="100px" height="100px"/>Mängin korvpalli</li>
+    <li>Käin jõusaalis raskusi tõstmas ja kenade tydrukutega tutvusi loomas</li>
+    <li>Mängin arvutimänge: väga meeldib <a href="http://origin.lolking.net/summoner/euw/20470609#profile">League of Legends</a> ja CS:GO</li>
+</ol>
+</div>
+<h4>Natuke minu eelistustest:</h4>
+    <ul>
+        <p id="lemmik">Lemmik värv roheline</p>
+        <br>
+        <br>
+        <ul>
+            <table width="500" border="3" cellpadding="5" style="border-color: green">
+                <th colspan="2">Lemmik näitlejad</th>
+
+                <tr>
+                    <td align="center" valign="center"><img src="http://www.bellazon.com/main/uploads/monthly_01_2008/post-774-1200395745.jpg" width="250px" height="200px" />
+                <br>
+                        <span style="cursor:not-allowed">Leonardo Dicaprio</span>
+                </td>
+                <td align="center" valign="center"><img src="http://cdn.wegotthiscovered.com/wp-content/uploads/url34.jpg" width="250px" height="200px"  />
+                    <br>
+                    <span style="cursor:not-allowed">Johnny Depp</span>
+                </td>
+                </tr>
+
+            </table>
+
+        </ul>
+        <p><strong>Lemmik filmid:</strong></p>
+        <div id="three"><ul class="aa">
+            <li>Mad Max</li>
+        <li>Avengers</li>
+        <li>Guardians of the Galaxy</li>
+    </ul>
+        </div>
+        </ul>
+<hr>
+<div>
+<p><img src="http://images.forum-auto.com/mesimages/575114/Opel%20Astra%20-%20Politsei%20Estonie%20-%20Rietze.jpg" width="200" height="150"/>Mulle meeldib
+    räpp. Kuulan pidevalt <a href="https://en.wikipedia.org/wiki/Eminem">Eminem</a>'i, <a href="https://en.wikipedia.org/wiki/Hopsin">Hopsin</a>'i,
+    <a href="https://en.wikipedia.org/wiki/Tech_N9ne">Tech n9ne</a>'i ja vahest <a href="https://en.wikipedia.org/wiki/Die_Antwoord">Die Antwoord</a>'i. Mul on
+    ema, isa, vanaema, kahjuks mul õdesid ja vendi pole. Tahan saada arvutigraafikuks või programmeerijaks.
+    Lapsena meeldis mulle mudelautodega mängida, sest arvutit mul polnud. Muidu olen tore ja sõbralik säga. </p>
+</div>
+</body>
+</html>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

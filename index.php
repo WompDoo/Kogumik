6d5bc9cd35406f9cf2f337ee367019dd21a53d2c
Janre <janre.press@khk.ee>
2015-09-16 18:19:52 +0300

    Avaleht [Delivers #102580766]

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <!--Avaleht rohelist v2rvi-->
 <body>
-<h1><img id="joint"  src="https://s-media-cache-ak0.pinimg.com/736x/e8/ea/0d/e8ea0d273f49a2c99eb88ab66b295fba.jpg" /><u>Heigo Kure Tutvustav Koduleht</u></h1>
+<h1><img id="joint"  src="https://s-media-cache-ak0.pinimg.com/736x/e8/ea/0d/e8ea0d273f49a2c99eb88ab66b295fba.jpg"  alt="Relevant textual alternative to the image"/><u>Heigo Kure Tutvustav Koduleht</u></h1>
 <h3><a href="https://www.facebook.com/heigo.kure?fref=ts" style=color:red>Heigo Kure FB</a></h3>
 <div id="one"><ul>
     <li>Olen sündinud 24.06.1995</li>
@@ -18,7 +18,7 @@
 </div>
 <h4>Hobid, tegevused:</h4>
 <div id="two"><ol>
-    <li><img id="korv" src="http://www.animatedimages.org/data/media/159/animated-basketball-image-0029.gif" />Mängin korvpalli</li>
+    <li><img id="korv" src="http://www.animatedimages.org/data/media/159/animated-basketball-image-0029.gif"  alt="Relevant textual alternative to the image"/>Mängin korvpalli</li>
     <li>Käin jõusaalis raskusi tõstmas ja kenade tüdrukutega tutvusi loomas</li>
     <li>Mängin arvutimänge: väga meeldib <a href="http://origin.lolking.net/summoner/euw/20470609#profile">League of Legends</a> ja CS:GO</li>
 </ol>
@@ -33,11 +33,11 @@
                 <th colspan="2">Lemmik näitlejad</th>
 
                 <tr>
-                    <td id="dic"><img id="leo" src="http://www.bellazon.com/main/uploads/monthly_01_2008/post-774-1200395745.jpg" />
+                    <td id="dic"><img id="leo" src="http://www.bellazon.com/main/uploads/monthly_01_2008/post-774-1200395745.jpg" alt="Relevant textual alternative to the image" />
                 <br>
                         <span style="cursor:not-allowed">Leonardo Dicaprio</span>
                 </td>
-                <td id="john"><img id="johhn" src="http://cdn.wegotthiscovered.com/wp-content/uploads/url34.jpg"  />
+                <td id="john"><img id="johhn" src="http://cdn.wegotthiscovered.com/wp-content/uploads/url34.jpg" alt="Relevant textual alternative to the image" />
                     <br>
                     <span style="cursor:not-allowed">Johnny Depp</span>
                 </td>
@@ -58,7 +58,7 @@
         </ul>
 <hr>
 <div>
-<p><img id="pol" src="http://images.forum-auto.com/mesimages/575114/Opel%20Astra%20-%20Politsei%20Estonie%20-%20Rietze.jpg"/>Mulle meeldib
+<p><img id="pol" src="http://images.forum-auto.com/mesimages/575114/Opel%20Astra%20-%20Politsei%20Estonie%20-%20Rietze.jpg" alt="Relevant textual alternative to the image"/>Mulle meeldib
     räpp. Kuulan pidevalt <a href="https://en.wikipedia.org/wiki/Eminem">Eminem</a>'i, <a href="https://en.wikipedia.org/wiki/Hopsin">Hopsin</a>'i,
     <a href="https://en.wikipedia.org/wiki/Tech_N9ne">Tech n9ne</a>'i ja vahest <a href="https://en.wikipedia.org/wiki/Die_Antwoord">Die Antwoord</a>'i. Mul on
     ema, isa, vanaema, kahjuks mul õdesid ja vendi pole. Tahan saada arvutigraafikuks või programmeerijaks.
